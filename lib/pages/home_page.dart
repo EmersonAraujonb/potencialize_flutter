180e3386a2dadadcef98212880e5c37e6d53dcb1
Emerson Araújo <emersonsantos0302@gmail.com>
2026-03-13 20:49:54 -0300

    criando menu com opcoes

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -20,28 +20,90 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Home'),
         actions: [
-          IconButton(
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (_) => const UserPage(),
-                ),
-              );
-            },
-            icon: CircleAvatar(
-              radius: 14,
-              backgroundImage: userAvatarUrl != null
-                  ? NetworkImage(userAvatarUrl)
-                  : null,
-              child: userAvatarUrl == null
-                  ? const Icon(Icons.person, size: 18)
-                  : null,
-            ),
-            tooltip: 'Usuário',
+          PopupMenuButton<String>(
+                icon: const Icon(Icons.more_vert_sharp, size: 28),
+                color: const Color.fromARGB(255, 147, 59, 247),
+                tooltip: 'Conta',
+
+                onSelected: (value) {
+                  if (value == 'perfil') {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const UserPage(),
+                      ),
+                    );
+                  }
+
+                  if (value == 'config') {
+                    print('Abrir configurações');
+                  }
+
+                  if(value == 'tema'){
+                    print('alternar tema');
+                  }
+
+                  if (value == 'sair') {
+                    print('Sair da conta');
+                  }
+                },
+
+                itemBuilder: (context) => [
+
+                  // PERFIL
+                  PopupMenuItem(
+                    value: 'perfil',
+                    child: Row(
+                      children: [
+
+                        CircleAvatar(
+                          radius: 12,
+                          backgroundImage: userAvatarUrl != null
+                              ? NetworkImage(userAvatarUrl)
+                              : null,
+                          child: userAvatarUrl == null
+                              ? const Icon(Icons.person, size: 16)
+                              : null,
+                        ),
+
+                        const SizedBox(width: 10),
+
+                        const Text('Perfil'),
+                      ],
+                    ),
+                  ),
+
+                  const PopupMenuDivider(),
+
+                  // CONFIGURAÇÕES
+                  const PopupMenuItem(
+                    value: 'config',
+                    child: Row(
+                      children: [
+                        Icon(Icons.settings),
+                        SizedBox(width: 10),
+                        Text('Configurações'),
+                      ],
+                    ),
+                  ),
+
+                  const PopupMenuDivider(),
+
+                  // SAIR
+                  const PopupMenuItem(
+                    value: 'sair',
+                    child: Row(
+                      children: [
+                        Icon(Icons.logout, color: Colors.red),
+                        SizedBox(width: 10),
+                        Text('Sair'),
+                      ],
+                    ),
+                  ),
+                ],
+              )
+            ],
           ),
-        ],
-      ),
       body: Center(
         child: Padding(
           padding: const EdgeInsets.all(24),
